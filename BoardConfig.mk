5116a072a1036fd40dde1f891dc9259f1fa1fdee
Cosmin Tanislav <demonsingur@gmail.com>
2022-05-09 20:49:48 +0300

    burton: use UDFPS extension
    Change-Id: I3a3f710dd7a911443925c2773c0b65da96feb1da

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,12 +4,15 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+COMMON_PATH := device/motorola/sm8250-common
+
 include device/motorola/sm8250-common/BoardConfig.mk
 
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := burton
 
 # Fingerprint
+TARGET_SURFACEFLINGER_UDFPS_LIB := //$(COMMON_PATH):libudfps_extension.moto_sm8250
 TARGET_USES_FOD_ZPOS := true
 
 # Kernel
